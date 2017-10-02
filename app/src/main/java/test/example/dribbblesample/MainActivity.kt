303031fdcc0e27c0647043f3d63edb86101bec5c
pankratov <pankratov@vigroup.ru>
2017-10-02 19:01:17 +0500

    initial commit

---
+++ app/src/main/java/test/example/dribbblesample/MainActivity.kt
@@ -0,0 +1,12 @@
+package test.example.dribbblesample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
